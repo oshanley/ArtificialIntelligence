7f684eaea72bcf67e60a6d357e175cdd8b5f0a1a
Olivia Shanley <olivia.shanley@gmail.com>
2017-02-10 23:18:51 -0500

    BFS finds goal

--- projects/SearchMap.java
+++ projects/SearchMap.java
@@ -1,96 +1,207 @@
 import java.util.*;
-import java.io.*;//FileInputStream;
-/*public enum SpaceType { COMMA, HASHTAG, PERIOD };
+import java.io.*;
 
-
-
-public void DFS (){
-    Queue<String> Frontier;
-
-}
-
-public void BFS (){
-    Queue<String> Frontier;
-
-}
-
-public void A_STAR(){
-    Queue <String> Frontier = new PriorityQueue();
-
-}
-
-public void initmap(int col, int row){
-
-
-}*/
 public class SearchMap{
 
     ArrayList<String> searchAlgs;
     ArrayList<File> maps;
 
-    public enum SpaceType {
-         COMMA, HASH, PERIOD
-     }
-
     private class Space {
-        SpaceType spacetype;
-        int cost;
-        boolean visited = false;
+        private char symbol;
+        private int cost;
+        private boolean visited = false;
+        private int[] coords;
 
-        private Space (SpaceType type, int cost, boolean beenVisited){
-            spacetype = type;
-            cost = cost;
-            visited = beenVisited;
+        private Space (char symbol, int[] coords, int cost, boolean beenVisited){
+            this.symbol = symbol;
+            this.coords = coords;
+            this.cost = cost;
+            this.visited = beenVisited;
         }
 
-        private boolean hasBeenVisited(){
+        public void visited(boolean update){
+            this.visited = update;
+        }
+
+        public int getRow(){
+            return coords[0];
+        }
+
+        public int getCol(){
+            return coords[1];
+        }
+
+        public boolean hasBeenVisited(){
             return visited;
         }
-        private SpaceType type(){
-            return spacetype;
+
+        public char symbol(){
+            return symbol;
         }
-        private int cost(){
+
+        public int cost(){
             return cost;
         }
 
     }
 
+    private int[] findStart(Space[][] map){
+        int[] startCoords = new int[2];
+        for (int row = 0; row < map.length; row++) {
+            for (int col=0; col < map[0].length; col++) {
+                if (map[row][col].symbol() == 's'){
+                    startCoords[0] = row;
+                    startCoords[1] = col;
+                }
+            }
+        }
+        return startCoords;
+    }
+
+    private ArrayList<Space> validNeighbors(Space[][] map, int curRow, int curCol){
+        ArrayList<Space> validMoves = new ArrayList<Space>();
+
+        if (curRow > 0){
+            Space up = map[curRow-1][curCol];
+            if(!(up.symbol() == '#') && !up.hasBeenVisited()){
+                System.out.println("Up is valid: (" + up.getRow() + "," + up.getCol() + ")");
+
+                validMoves.add(up);
+                up.visited = true;
+            }
+        }
+        if(curRow < map.length-1){
+            Space down = map[curRow+1][curCol];
+            if(!(down.symbol() == '#') && !down.hasBeenVisited()){
+                System.out.println("Down is valid: (" + down.getRow() + "," + down.getCol() + ")");
+
+                validMoves.add(down);
+                down.visited = true;
+            }
+        }
+        if(curCol > 0){
+            Space left = map[curRow][curCol-1];
+            if(!(left.symbol() == '#') && !left.hasBeenVisited()){
+                System.out.println("Left is valid: (" + left.getRow() + "," + left.getCol() + ")");
+
+                validMoves.add(left);
+                left.visited = true;
+            }
+        }
+        if(curCol < map[0].length-1){
+            Space right = map[curRow][curCol+1];
+            if(!(right.symbol() == '#') && !right.hasBeenVisited()){
+                System.out.println("Right is valid: (" + right.getRow() + "," + (right.getCol()) + ")");
+
+                validMoves.add(right);
+                right.visited = true;
+            }
+        }
+
+        return validMoves;
+    }
+
+    private void breadthFirstSearch(Space[][] map){
+        Queue <Space> frontier = new LinkedList<Space>();
+        int [] start = findStart(map);
+
+        System.out.println("Start row: " + start[0] + " col: " + start[1]);
+
+        boolean goal = false;
+        int row = start[0];
+        int col = start[1];
+
+        Space startSpace = map[row][col];
+        frontier.add(startSpace);
+        startSpace.visited = true;
+
+        while (!goal){
+            Space curSpace = frontier.peek();
+            row = curSpace.getRow();
+            col =  curSpace.getCol();
+            //System.out.println("Current space: (" + row + "," + col + ")");
+
+            ArrayList <Space> validMoves = validNeighbors(map, row, col);
+            //System.out.println("Adding to frontier: ");
+
+            for (Space sp : validMoves ) {
+                System.out.println("(" + sp.getRow() + "," + sp.getCol() + ")");
+                frontier.add(sp);
+            }
+
+            Space rm = frontier.remove();
+            //System.out.println("Checking if  to frontier: (" + rm.getRow() + "," + rm.getCol() + ") is goal.");
+            //System.out.println("Symbol is: " + rm.symbol());
+
+            if (rm.symbol() == 'g') {
+                goal = true;
+                System.out.println("GOAL FOUND AT: (" + row + "," + col + ")");
+            }
+            else{
+                if (frontier.peek() == null){
+                    System.out.println("This grid has no solution. Exiting.");
+                    return;
+                }
+                //System.out.println("Next to evaluate: (" + frontier.peek().getRow() + "," + frontier.peek().getCol() + ")");
+
+
+                // if (row < map.length-1){
+                //     if (col == map[0].length){
+                //         col = 0;
+                //         row++;
+                //     }
+                //     else{
+                //         col++;
+                //         row++;
+                //     }
+                // }
+                // else{
+                //     System.out.println("This grid has no solution. Exiting.");
+                //     return;
+                // }
+            }
+        }
+
+    }
+
     private Space[][] parseMap(File file){
-        Space map[][] = null;
+        Space [][] map = null;
         try{
             FileReader fr = new FileReader(file); //read file input
             BufferedReader br = new BufferedReader(fr); //buffer input
             String[] dimensions = br.readLine().split("\\s"); //read first line
 
             //read dimensions
-            int rowSize = Integer.parseInt(dimensions[0]);
-            int colSize = Integer.parseInt(dimensions[1]);
+            int colSize = Integer.parseInt(dimensions[0]);
+            int rowSize = Integer.parseInt(dimensions[1]);
             map = new Space[rowSize][colSize];
-
-            System.out.println("Rows: " + rowSize + " Cols: " + colSize + "\n");
 
             int row = 0;
             int col = 0;
             String line;
+
             while((line = br.readLine()) != null){
                 for(char c : line.toCharArray()){
-                    SpaceType type = null;
-                    int cost = 0;
+                    int cost = 1;
                     if (c == '#') {
-                        type = SpaceType.HASH;
                         cost = 0;
                     }
                     else if (c == '.') {
-                        type = SpaceType.PERIOD;
                         cost = 1;
                     }
                     else if (c == ','){
-                        type = SpaceType.COMMA;
                         cost = 2;
                     }
-                    map[row][col] = new Space(type, cost, false);
-                    System.out.print(c + "   ");
+                    int[] coords = new int[2];
+                    coords[0] = row;
+                    coords[1] = col;
+
+                    map[row][col] = new Space(c, coords, cost, false);
+                    System.out.print(map[row][col].symbol() + "   ");
+                    col++;
                 }
+                row++;
+                col = 0;
                 System.out.println();
             }
             fr.close();
@@ -143,11 +254,6 @@ public class SearchMap{
         return new File(userInput);
     }
 
-    private void search(){
-        String algorithm = promptForAlg();
-        File map = chooseMap();
-    }
-
     public SearchMap() {
         searchAlgs = new ArrayList<>(Arrays.asList("DFS", "BFS", "A*"));
         maps = new ArrayList<File>(Arrays.asList(new File("./maps").listFiles()));
@@ -155,8 +261,18 @@ public class SearchMap{
 
     public static void main(String[] args) {
         SearchMap search = new SearchMap();
-        search.promptForAlg();
+        String searchType = search.promptForAlg();
         File mapFile = search.chooseMap();
-        search.parseMap(mapFile);
+        Space[][] map = search.parseMap(mapFile);
+
+        switch (searchType) {
+            case "DFS":
+                break;
+            case "BFS":
+                search.breadthFirstSearch(map);
+                break;
+            case "A*":
+                break;
+        }
     }
 }
